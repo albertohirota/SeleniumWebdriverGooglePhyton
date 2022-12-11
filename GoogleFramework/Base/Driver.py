2046dec20acc4bb4fcda310f61eebc9e7f97029b
Alberto Hirota <alberto.hirota@gmail.com>
2022-12-11 10:51:43 -0500

    Create Chrome Instance

--- GoogleFramework/Base/Driver.py
+++ GoogleFramework/Base/Driver.py
@@ -1,6 +1,46 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeServices
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service as EdgeServices
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxServices
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from webdriver_manager.firefox import GeckoDriverManager 
+from enum import Enum
+import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager.install()))
+class Browser(Enum):
+    CHROME = 1
+    EDGE = 2
+    FIREFOX = 3
+
+class Driver:
+    def __init__(self, browser) -> None:
+        self.browser = browser
+
+    def Initialize(browser):
+        match browser:
+            case Browser.CHROME:
+                options = ChromeOptions()
+                driver = webdriver.Chrome(service=ChromeServices(ChromeDriverManager().install()),options=options)
+                driver.maximize_window()
+                driver.get("https://google.com")
+            case Browser.EDGE:
+                print(Browser.EDGE)
+            case Browser.FIREFOX:
+                print (Browser.FIREFOX)
+            case __:
+                print("Error in the Browser")
+        time.sleep(5)
+        #TODO: Add logging here
+        
+        
+
+print("Test")
+Driver.Initialize(Browser.CHROME)
+    
+
+
+    
